7e8134ef0ec9410d903167c99902f5c0499c2c89
Sohaib <sohaibahmed.hypersoft@gmail.com>
2023-07-05 15:04:53 +0500

    CMP

--- app/src/main/java/dev/pegasus/cmp/MainActivity.kt
+++ app/src/main/java/dev/pegasus/cmp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import dev.pegasus.cmp.managers.ConsentManager
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/dev/pegasus/cmp/managers/ConsentManager.kt
+++ app/src/main/java/dev/pegasus/cmp/managers/ConsentManager.kt
@@ -3,11 +3,10 @@ package dev.pegasus.cmp.managers
 import android.app.Activity
 import android.util.Log
 import com.google.android.ump.ConsentDebugSettings
+import com.google.android.ump.ConsentForm
 import com.google.android.ump.ConsentInformation
-import com.google.android.ump.ConsentInformation.PrivacyOptionsRequirementStatus
 import com.google.android.ump.ConsentRequestParameters
 import com.google.android.ump.UserMessagingPlatform
-import dev.pegasus.cmp.interfaces.OnConsentResponse
 
 /**
  * @Author: SOHAIB AHMED
@@ -20,15 +19,14 @@ import dev.pegasus.cmp.interfaces.OnConsentResponse
 class ConsentManager(private val activity: Activity) {
 
     private var consentInformation: ConsentInformation? = null
-    private var onConsentResponse: OnConsentResponse? = null
+    private var consentForm: ConsentForm? = null
+    private var callback: ((errorMessage: String?) -> Unit)? = null
 
-    val canRequestAds: Boolean get() = consentInformation?.canRequestAds() ?: false
-
-    fun initDebugConsent(deviceId: String, onConsentResponse: OnConsentResponse) {
-        this.onConsentResponse = onConsentResponse
+    fun initDebugConsent(callback: (errorMessage: String?) -> Unit) {
+        this.callback = callback
         val debugSettings = ConsentDebugSettings.Builder(activity)
             .setDebugGeography(ConsentDebugSettings.DebugGeography.DEBUG_GEOGRAPHY_EEA)
-            .addTestDeviceHashedId(deviceId)
+            .addTestDeviceHashedId("7ECB24A02579DF622CC359AF4968D9CC")
             .build()
 
         val params = ConsentRequestParameters
@@ -41,49 +39,56 @@ class ConsentManager(private val activity: Activity) {
             it.requestConsentInfoUpdate(activity, params, {
                 // The consent information state was updated.
                 // You are now ready to check if a form is available.
-                Log.d("ConsentManager", "initDebugConsent: Available")
+                Log.d("TAG", "initDebugConsent: Available")
                 loadForm()
             }, { error ->
                 // Handle the error.
-                Log.e("ConsentManager", "initDebugConsent: $error")
-                onConsentResponse.onResponse(error.message)
+                Log.e("TAG", "initDebugConsent: $error")
+                callback.invoke(error.message)
             })
         }
     }
 
-    fun initConsent(onConsentResponse: OnConsentResponse) {
-        this.onConsentResponse = onConsentResponse
+    fun initConsent(callback: (errorMessage: String?) -> Unit) {
+        this.callback = callback
         val params = ConsentRequestParameters.Builder().setTagForUnderAgeOfConsent(false).build()
 
         consentInformation = UserMessagingPlatform.getConsentInformation(activity).also {
             it.requestConsentInfoUpdate(activity, params, {
                 if (it.isConsentFormAvailable) {
-                    Log.d("ConsentManager", "initConsent: Available")
+                    Log.d("TAG", "initConsent: Available")
                     loadForm()
                 } else {
-                    onConsentResponse.onResponse()
+                    callback.invoke(null)
                 }
             }, { error ->
-                Log.e("ConsentManager", "initConsent: $error")
-                onConsentResponse.onResponse(error.message)
+                Log.e("TAG", "initConsent: $error")
+                callback.invoke(error.message)
             })
         }
     }
 
     private fun loadForm() {
         // Loads a consent form. Must be called on the main thread.
-        UserMessagingPlatform.loadAndShowConsentFormIfRequired(activity) { formError ->
-            formError?.let {
-                onConsentResponse?.onResponse(it.message)
-            } ?: run {
-                onConsentResponse?.onResponse()
-                checkForPrivacyOptions()
+        UserMessagingPlatform.loadConsentForm(activity, {
+            this.consentForm = it
+            if (consentInformation?.consentStatus == ConsentInformation.ConsentStatus.REQUIRED) {
+                Log.d("TAG", "loadForm: showing")
+                it.show(activity) {
+                    if (consentInformation?.consentStatus == ConsentInformation.ConsentStatus.OBTAINED) {
+                        // App can start requesting ads.
+                        callback?.invoke("Obtained, now we can fetch ads")
+                        return@show
+                    }
+                    callback?.invoke("Failed! Trying again")
+                }
+            } else {
+                callback?.invoke("Consent form not required")
             }
-        }
-    }
-
-    private fun checkForPrivacyOptions() {
-        val isRequired = consentInformation?.privacyOptionsRequirementStatus == PrivacyOptionsRequirementStatus.REQUIRED
-        onConsentResponse?.onPolicyRequired(isRequired)
+        }, { error ->
+            // Handle the error.
+            Log.e("TAG", "loadForm: ${error.message}")
+            callback?.invoke(error.message)
+        })
     }
 }
